e1b40d3e9eb5c3c8d21f48f81eb6dd83d92dd0e6
suryawanshishivani <shivanisuryawanshi10@gmail.com>
2023-09-13 12:48:02 +0530

    Third commit

---
+++ domm.js
@@ -0,0 +1,48 @@
+//EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document);
+//console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+
+//GETELEMENTBYID
+//console.log(document.getElementById('header title'));
+ //let headerTitle=document.getElementById('header title');
+ let header=document.getElementById('main header')
+ // console.log(headerTitle);
+ // //headerTitle.textContent='helooo'
+ // headerTitle.innerText='Item Lister'
+ header.style.borderBottom ='solid 6px #000';
+ 
+ 
+ //GETELEMENTSBYCLASSNAME
+
+ 
+  let Item=document.getElementsByClassName('list-group-item');
+  //console.log(Item);
+  //console.log(Item[1]);
+ //Item[1].textContent='hello my lady'
+ //console.log(Item[1].textContent);
+ Item[2].style.backgroundColor='green';
+
+ for(let i=0;i<=Item.length;i++){
+    Item[i].style.fontWeight='bold'
+ }
+
+ // Item[1].style.fontWeight='bold'
+ 
+ // //Item.style.backgroundColor='#f4f4f4';//It does not happen for that we have to put it into loop
+ 
+ // for(let i=0;i<=Item.length;i++){
+ //     Item[i].style.backgroundColor='#f4f4f4'
+ //     Item[i].style.fontWeight='Italic'
+ 
+ // }
+//  let headerTi=document.getElementsByClassName('bg');
+//  headerTi.style.borderBottom='solid 5px #000';
+ 
